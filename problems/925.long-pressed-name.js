323609e256ff3e098aefb3bd5cf6492e903860c7
Kohei Tanaka <kohei69@gmail.com>
2021-03-11 19:14:50 +0900

    WIP

---
+++ problems/925.long-pressed-name.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=925 lang=javascript
+ *
+ * [925] Long Pressed Name
+ */
+
+// @lc code=start
+/**
+ * @param {string} name
+ * @param {string} typed
+ * @return {boolean}
+ */
+var isLongPressedName = function(name, typed) {
+};
+// @lc code=end
+
